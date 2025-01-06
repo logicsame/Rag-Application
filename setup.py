81bcd3bf69b92b6f44b3564430e9aabf5fc1270a
logicsame <useforprofessional@gmail.com>
2025-01-07 01:27:05 +0600

    research updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Rag Application',
+    version='0.0.0',
+    author='logicsame',
+    author_email='useforprofessional@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
